2dc9d7f5c964797423e93fb5ac6608aa6ea1c7fd
Andrii Gadar <gadar.andre@gmail.com>
2022-12-02 11:07:35 +0200

    Внесені зміни до modal-windows

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,26 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('backdrop--is-hidden');
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("backdrop--is-hidden");
   }
 })();
 
 (() => {
-  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
-    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
 
     e.currentTarget.reset();
   });
